544ca11b95e46f3ee3ae808ade1b6de6e576827d
Ian G. Clifton <IanGClifton@users.noreply.github.com>
2020-02-14 10:47:10 -0800

    Added placeholders for preferences
    This uses the standard Leanback approach to preferences where there is a
    LeanbackSettingsFragment(Compat) that serves as the root for all the
    individual preference fragments. Right now, there's just one and it just
    pops up a Toast when you interact with things, but that should make it
    trivial to add others and handle interactions.
    
    You can get to the preferences from the BrowseFragment using
    BrowseFragmentDirections on the nav controller (similar to going to the
    PlaybackFragment). Once the BrowseFragment has some content, we can put
    a specific prefs item in it.
    
    Change-Id: Ic2903d52b1538c9f226f451df16f5c9c45e096d1

---
+++ ReferenceAppKotlin/app/src/main/java/com/android/tv/reference/preferences/PreferencesContainerFragment.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tv.reference.preferences
+
+import androidx.leanback.preference.LeanbackSettingsFragmentCompat
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceScreen
+
+/**
+ * Base Fragment that serves as an entry point into settings
+ */
+class PreferencesContainerFragment : LeanbackSettingsFragmentCompat() {
+
+    override fun onPreferenceStartInitialScreen() {
+        startPreferenceFragment(PreferencesFragment())
+    }
+
+    override fun onPreferenceStartScreen(
+        caller: PreferenceFragmentCompat?,
+        pref: PreferenceScreen?
+    ): Boolean {
+        return false
+    }
+
+    override fun onPreferenceStartFragment(
+        caller: PreferenceFragmentCompat?,
+        pref: Preference?
+    ): Boolean {
+        return false
+    }
+}
---
+++ ReferenceAppKotlin/app/src/main/java/com/android/tv/reference/preferences/PreferencesFragment.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tv.reference.preferences
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.android.tv.reference.R
+
+/**
+ * Fragment for displaying the default settings screen
+ *
+ * TODO Update to LeanbackPreferenceFragmentCompat and handle interactions
+ */
+class PreferencesFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference?): Boolean {
+        Toast.makeText(requireContext(), "TODO: Handle preference click", Toast.LENGTH_SHORT).show()
+        return true
+    }
+}
