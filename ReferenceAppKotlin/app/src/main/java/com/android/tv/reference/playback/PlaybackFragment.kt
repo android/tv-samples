53c38a0a4a675fbb82880f49f1e3a851aef85d5b
Ian G. Clifton <IanGClifton@users.noreply.github.com>
2020-02-14 16:07:48 -0800

    Added very basic playback
    This just plays the given video. Play and pause should work but nothing
    else is connected yet. Media Session is not yet integrated either.
    
    ExoPlayer is also now using the latest version.
    
    Change-Id: I0f9acd9cfd5f1f0957ea09847885f5a189334a89

--- ReferenceAppKotlin/app/src/main/java/com/android/tv/reference/playback/PlaybackFragment.kt
+++ ReferenceAppKotlin/app/src/main/java/com/android/tv/reference/playback/PlaybackFragment.kt
@@ -16,9 +16,19 @@
 
 package com.android.tv.reference.playback
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.leanback.app.VideoSupportFragment
+import androidx.leanback.app.VideoSupportFragmentGlueHost
+import androidx.leanback.media.PlaybackTransportControlGlue
+import com.android.tv.reference.R
 import com.android.tv.reference.shared.datamodel.Video
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ext.leanback.LeanbackPlayerAdapter
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 
 /**
  * Fragment that plays video content with ExoPlayer
@@ -27,9 +37,46 @@ class PlaybackFragment : VideoSupportFragment() {
 
     private lateinit var video: Video
 
+    private lateinit var exoplayer: ExoPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         video = PlaybackFragmentArgs.fromBundle(requireArguments()).video
+
+        preparePlayer()
+        prepareGlue()
+        prepareMediaSession()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        exoplayer.release()
+    }
+
+    private fun preparePlayer() {
+        val dataSourceFactory = DefaultDataSourceFactory(requireContext(), Util.getUserAgent(requireContext(), getString(
+            R.string.app_name)))
+        val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse(video.videoUri))
+        exoplayer = SimpleExoPlayer.Builder(requireContext()).build().apply {
+            prepare(mediaSource)
+            playWhenReady = true
+        }
+    }
+
+    private fun prepareGlue() {
+        PlaybackTransportControlGlue(requireContext(), LeanbackPlayerAdapter(requireContext(), exoplayer, PLAYER_UPDATE_INTERVAL_MILLIS)).apply {
+            host = VideoSupportFragmentGlueHost(this@PlaybackFragment)
+            title = video.name
+        }
+    }
+
+    private fun prepareMediaSession() {
+        // TODO
+    }
+
+    companion object {
+        // How often to update the player UI
+        private const val PLAYER_UPDATE_INTERVAL_MILLIS = 250
     }
 }
