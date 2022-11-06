ec0fd6123ad61545ca80693664d9c9d33f01d509
Masoud <masoud.mokhtari69@gmail.com>
2022-11-06 15:23:56 +0330

    Initial changes.

---
+++ Leanback/app/src/main/java/com/example/android/tvleanback/presenter/IconRowHeaderPresenter.kt
@@ -0,0 +1,29 @@
+package com.example.android.tvleanback.presenter
+
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.leanback.widget.Presenter
+import androidx.leanback.widget.RowHeaderPresenter
+import com.example.android.tvleanback.interfaces.HasHeader
+import com.example.android.tvleanback.databinding.IconRowHeaderBinding
+
+class IconRowHeaderPresenter : RowHeaderPresenter() {
+    class ViewHolder(val binding: IconRowHeaderBinding) : RowHeaderPresenter.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup): Presenter.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = IconRowHeaderBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(viewHolder: Presenter.ViewHolder, item: Any) {
+        require(viewHolder is ViewHolder)
+        require(item is HasHeader)
+        item.header.iconRes?.let {
+            val icon: Drawable =
+                viewHolder.view.resources.getDrawable(it, null)
+            viewHolder.binding.icon.setImageDrawable(icon)
+        }
+    }
+}
