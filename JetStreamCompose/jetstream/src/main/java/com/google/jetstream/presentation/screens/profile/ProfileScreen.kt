952b043e6734b75e8dd0761fd7ba236bb2ffb24c
Tomáš Mlynarič <mlykotom@google.com>
2023-07-26 13:51:58 +0200

    Scroll profile column

--- JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/screens/profile/ProfileScreen.kt
+++ JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/screens/profile/ProfileScreen.kt
@@ -29,6 +29,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -98,6 +100,7 @@ fun ProfileScreen(
         Column(
             modifier = Modifier
                 .fillMaxWidth(fraction = sidebarWidthFraction)
+                .verticalScroll(rememberScrollState())
                 .fillMaxHeight()
                 .onFocusChanged {
                     isLeftColumnFocused = it.hasFocus
