d87a583725bbc34ddd6798d222104c366e7a7c37
Paul Lammertsma <plammer@google.com>
2024-02-23 10:13:19 -0500

    add a bit of window inset to prevent the focus from jumping to the top of the screen while navigating to the right

--- TvMaterialCatalog/app/src/main/java/com/google/tv/material/catalog/AppBar.kt
+++ TvMaterialCatalog/app/src/main/java/com/google/tv/material/catalog/AppBar.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -129,7 +131,10 @@ private fun Actions(
         )
     )
 
-    Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier.windowInsetsPadding(WindowInsets(0.dp, 0.dp, 8.dp, 0.dp))
+    ) {
         actions.forEach {
             Button(onClick = it.onClick) {
                 Icon(
