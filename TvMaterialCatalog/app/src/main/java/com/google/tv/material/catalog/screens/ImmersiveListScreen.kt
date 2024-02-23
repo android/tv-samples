d3ca67cd07628a9ebcf1633ef83b678efbc7299c
Paul Lammertsma <plammer@google.com>
2024-02-23 10:00:06 -0500

    row should span width

--- TvMaterialCatalog/app/src/main/java/com/google/tv/material/catalog/screens/ImmersiveListScreen.kt
+++ TvMaterialCatalog/app/src/main/java/com/google/tv/material/catalog/screens/ImmersiveListScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -99,6 +100,7 @@ fun ImmersiveListScreen() {
         TvLazyRow(
             modifier = Modifier
                 .align(Alignment.BottomStart)
+                .fillMaxWidth()
                 .padding(bottom = 20.dp)
                 .focusRestorer { firstChildFr },
             contentPadding = PaddingValues(start = 58.dp),
