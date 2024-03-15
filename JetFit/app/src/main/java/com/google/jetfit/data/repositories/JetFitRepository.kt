185cb90169b3ee373e6eb102dc894ce331171864
Abdurrahman <abdosalahas.01128@gmail.com>
2024-03-15 16:44:40 +0200

    add fake data

--- JetFit/app/src/main/java/com/google/jetfit/data/repositories/JetFitRepository.kt
+++ JetFit/app/src/main/java/com/google/jetfit/data/repositories/JetFitRepository.kt
@@ -1,7 +1,17 @@
 package com.google.jetfit.data.repositories
 
-interface JetFitRepository {
-    fun getWorkouts()
-    fun getWorkoutById()
+import com.google.jetfit.data.entities.Challenge
+import com.google.jetfit.data.entities.Routine
+import com.google.jetfit.data.entities.Series
+import com.google.jetfit.data.entities.Workout
 
+interface JetFitRepository {
+    fun getWorkouts(): List<Workout>
+    fun getWorkoutById(id: String): Workout
+    fun getSeries(): List<Series>
+    fun getSeriesById(id: String): Series
+    fun getRoutines(): List<Routine>
+    fun getRoutineById(id: String): Routine
+    fun getChallenges(): List<Challenge>
+    fun getChallengeById(id: String): Challenge
 }
