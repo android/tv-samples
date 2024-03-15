059cc0734f94df6205265d1ad095c3cfc199a656
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-03-15 11:47:07 +0200

    finish VideoPlayerControlsIcon composable function

--- JetFit/app/src/main/java/com/google/jetfit/presentation/screens/video_player/composable/VideoPlayerControlsIcon.kt
+++ JetFit/app/src/main/java/com/google/jetfit/presentation/screens/video_player/composable/VideoPlayerControlsIcon.kt
@@ -1,26 +1,35 @@
 package com.google.jetfit.presentation.screens.video_player.composable
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.tv.material3.Border
 import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.tv.material3.IconButtonDefaults
+import androidx.tv.material3.MaterialTheme
+import com.google.jetfit.R
 import com.google.jetfit.components.CustomFillIconButton
+import com.google.jetfit.presentation.theme.JetFitTheme
 
 @OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
 internal fun VideoPlayerControlsIcon(
-    modifier: Modifier = Modifier,
     state: VideoPlayerState,
     isPlaying: Boolean,
     icon: Int,
-    onClick: () -> Unit = {}
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
 ) {
     val interactionSource = remember { MutableInteractionSource() }
     val isFocused by interactionSource.collectIsFocusedAsState()
@@ -35,6 +44,23 @@ internal fun VideoPlayerControlsIcon(
         modifier = modifier.size(40.dp),
         onClick = onClick,
         icon = icon,
-        buttonColor = Color.Transparent
+        buttonColor = Color.Transparent,
+        border = IconButtonDefaults.border(
+            border = Border(
+                border = BorderStroke(1.dp, MaterialTheme.colorScheme.border),
+                shape = CircleShape
+            )
+        )
     )
+}
+
+
+@Preview(device = Devices.TV_1080p)
+@Composable
+fun PreviewVideoPlayerControlsIcon() {
+    JetFitTheme {
+        val state = rememberVideoPlayerState()
+        VideoPlayerControlsIcon(state = state, isPlaying = true, icon = R.drawable.subtitles) {
+        }
+    }
 }
