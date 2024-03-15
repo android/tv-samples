ec42013cca6f4b8a98825d529327ca8730e85257
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-03-15 13:40:14 +0200

    add preview function to VideoPlayerSeeker

--- JetFit/app/src/main/java/com/google/jetfit/presentation/screens/video_player/composable/VideoPlayerSeeker.kt
+++ JetFit/app/src/main/java/com/google/jetfit/presentation/screens/video_player/composable/VideoPlayerSeeker.kt
@@ -1,10 +1,16 @@
 package com.google.jetfit.presentation.screens.video_player.composable
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.jetfit.presentation.theme.JetFitTheme
+import com.google.jetfit.presentation.utils.padStartWith0
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
 @Composable
 fun VideoPlayerSeeker(
@@ -16,28 +22,43 @@ fun VideoPlayerSeeker(
 ) {
     val textProgress = contentProgress.toComponents { h, m, s, _ ->
         if (h > 0) {
-            "$h:${m}:${s}"
+            "$h:${m.padStartWith0()}:${s.padStartWith0()}"
         } else {
-            ":${m}:${s}"
+            "${m.padStartWith0()}:${s.padStartWith0()}"
         }
 
     }
     val textDuration = contentDuration.toComponents { h, m, s, _ ->
         if (h > 0) {
-            "$h:${m}:${s}"
+            "$h:${m.padStartWith0()}:${s.padStartWith0()}"
         } else {
-            ":${m}:${s}"
+            "${m.padStartWith0()}:${s.padStartWith0()}"
         }
     }
     Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically
+        modifier = modifier, verticalAlignment = Alignment.CenterVertically
     ) {
         VideoPlayerControllerIndicator(
-            progress = (contentProgress / contentDuration).toFloat(),
-            onSeek = onSeek,
-            state = state
+            progress = (contentProgress / contentDuration).toFloat(), onSeek = onSeek, state = state
         )
-        VideoPlayerDurationText(textProgress = textProgress, textDuration = textDuration)
+        VideoPlayerDurationText(
+            modifier = Modifier.padding(
+                horizontal = 8.dp
+            ), textProgress = textProgress, textDuration = textDuration
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewVideoPlayerSeeker() {
+    JetFitTheme {
+        VideoPlayerSeeker(
+            state = rememberVideoPlayerState(),
+            onSeek = {},
+            contentProgress = (5L).milliseconds,
+            contentDuration = (50L).milliseconds
+        )
     }
 }
