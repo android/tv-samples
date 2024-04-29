1f3ef36a9bcb80654f2850a2d1d1be5751cd4f5f
Paul Lammertsma <plammer@google.com>
2024-04-29 17:19:10 +0200

    fixed missing imports

--- TvMaterialCatalog/app/src/main/java/com/google/tv/material/catalog/HomeGrid.kt
+++ TvMaterialCatalog/app/src/main/java/com/google/tv/material/catalog/HomeGrid.kt
@@ -10,6 +10,9 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.focusRestorer
 import androidx.compose.ui.unit.dp
 import androidx.tv.foundation.lazy.grid.TvGridCells
 import androidx.tv.foundation.lazy.grid.TvGridItemSpan
