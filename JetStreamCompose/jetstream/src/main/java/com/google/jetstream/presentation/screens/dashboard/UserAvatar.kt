cbae28a2aeef7f9723a3031ccb3b487457686e4b
Chiko Shimizu <chikoski@google.com>
2024-05-15 13:20:31 +0900

    Update code due to the rename of the ToogleableSurfaceDefaults object

--- JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/screens/dashboard/UserAvatar.kt
+++ JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/screens/dashboard/UserAvatar.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * https://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,22 +18,18 @@ package com.google.jetstream.presentation.screens.dashboard
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.tv.material3.Border
-import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Icon
 import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.SelectableSurfaceDefaults
 import androidx.tv.material3.Surface
-import androidx.tv.material3.ToggleableSurfaceDefaults
 import com.google.jetstream.presentation.theme.JetStreamBorderWidth
 
-@OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
 fun UserAvatar(
     selected: Boolean,
@@ -41,8 +37,10 @@ fun UserAvatar(
     onClick: () -> Unit
 ) {
     Surface(
-        shape = ToggleableSurfaceDefaults.shape(shape = CircleShape),
-        border = ToggleableSurfaceDefaults.border(
+        selected = selected,
+        onClick = onClick,
+        shape = SelectableSurfaceDefaults.shape(shape = CircleShape),
+        border = SelectableSurfaceDefaults.border(
             focusedBorder = Border(
                 border = BorderStroke(
                     width = JetStreamBorderWidth,
@@ -58,10 +56,8 @@ fun UserAvatar(
                 shape = CircleShape
             ),
         ),
-        scale = ToggleableSurfaceDefaults.scale(focusedScale = 1f),
-        modifier = modifier.size(32.dp),
-        checked = selected,
-        onCheckedChange = { onClick() }
+        scale = SelectableSurfaceDefaults.scale(focusedScale = 1f),
+        modifier = modifier,
     ) {
         Icon(
             imageVector = Icons.Default.AccountCircle,
