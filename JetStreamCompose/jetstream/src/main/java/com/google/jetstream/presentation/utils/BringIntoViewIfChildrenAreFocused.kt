3d77121d2bd244b6bed1ac076665ba872ee707a3
Chiko Shimizu <chikoski@google.com>
2024-05-15 14:57:59 +0900

    Replace ImmersiveList with custom implementation as ImmersiveList is deprecated

--- JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/utils/BringIntoViewIfChildrenAreFocused.kt
+++ JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/utils/BringIntoViewIfChildrenAreFocused.kt
@@ -17,39 +17,50 @@
 package com.google.jetstream.presentation.utils
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.relocation.BringIntoViewResponder
 import androidx.compose.foundation.relocation.bringIntoViewResponder
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.debugInspectorInfo
+import androidx.compose.ui.unit.toSize
 
 @Suppress("IllegalExperimentalApiUsage") // TODO (b/233188423): Address before moving to beta
 @OptIn(ExperimentalFoundationApi::class)
-internal fun Modifier.bringIntoViewIfChildrenAreFocused(): Modifier = composed(
+internal fun Modifier.bringIntoViewIfChildrenAreFocused(
+    paddingValues: PaddingValues = PaddingValues()
+): Modifier = composed(
     inspectorInfo = debugInspectorInfo { name = "bringIntoViewIfChildrenAreFocused" },
     factory = {
+        val pxOffset = with(LocalDensity.current) {
+            val y = (paddingValues.calculateBottomPadding() - paddingValues.calculateTopPadding())
+                .toPx()
+            Offset.Zero.copy(y = y)
+        }
         var myRect: Rect = Rect.Zero
+        val responder = object : BringIntoViewResponder {
+            // return the current rectangle and ignoring the child rectangle received.
+            @ExperimentalFoundationApi
+            override fun calculateRectForParent(localRect: Rect): Rect {
+                return myRect
+            }
+
+            // The container is not expected to be scrollable. Hence the child is
+            // already in view with respect to the container.
+            @ExperimentalFoundationApi
+            override suspend fun bringChildIntoView(localRect: () -> Rect?) {
+            }
+        }
+
         this
             .onSizeChanged {
-                myRect = Rect(Offset.Zero, Offset(it.width.toFloat(), it.height.toFloat()))
+                val size = it.toSize()
+                myRect = Rect(pxOffset, size)
             }
-            .bringIntoViewResponder(
-                remember {
-                    object : BringIntoViewResponder {
-                        // return the current rectangle and ignoring the child rectangle received.
-                        @ExperimentalFoundationApi
-                        override fun calculateRectForParent(localRect: Rect): Rect = myRect
-
-                        // The container is not expected to be scrollable. Hence the child is
-                        // already in view with respect to the container.
-                        @ExperimentalFoundationApi
-                        override suspend fun bringChildIntoView(localRect: () -> Rect?) {}
-                    }
-                }
-            )
+            .bringIntoViewResponder(responder)
     }
 )
