715475937ff40399613d9772839bafad1ace933a
Chiko Shimizu <chikoski@google.com>
2024-05-16 11:41:20 +0900

    code cleanup

--- JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/screens/home/Top10MoviesList.kt
+++ JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/screens/home/Top10MoviesList.kt
@@ -38,6 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
@@ -72,10 +73,40 @@ fun Top10MoviesList(
         stringResource(R.string.top_10_movies_title)
     }
 
+    ImmersiveList(
+        selectedMovie = selectedMovie,
+        isListFocused = isListFocused,
+        gradientColor = gradientColor,
+        movieList = movieList,
+        sectionTitle = sectionTitle,
+        onMovieClick = onMovieClick,
+        onMovieFocused = {
+            selectedMovie = it
+        },
+        onFocusChanged = {
+            isListFocused = it.hasFocus
+        },
+        modifier = modifier.bringIntoViewIfChildrenAreFocused(
+            PaddingValues(bottom = 116.dp)
+        )
+    )
+}
+
+@Composable
+private fun ImmersiveList(
+    selectedMovie: Movie,
+    isListFocused: Boolean,
+    gradientColor: Color,
+    movieList: MovieList,
+    sectionTitle: String?,
+    onFocusChanged: (FocusState) -> Unit,
+    onMovieFocused: (Movie) -> Unit,
+    onMovieClick: (Movie) -> Unit,
+    modifier: Modifier = Modifier,
+) {
     Box(
         contentAlignment = Alignment.BottomStart,
         modifier = modifier
-            .bringIntoViewIfChildrenAreFocused(paddingValues = PaddingValues(bottom = 96.dp))
     ) {
         Background(
             movie = selectedMovie,
@@ -90,7 +121,7 @@ fun Top10MoviesList(
                     movie = selectedMovie,
                     modifier = Modifier.padding(
                         start = rememberChildPadding().start,
-                        bottom = 32.dp
+                        bottom = 40.dp
                     )
                 )
             }
@@ -102,10 +133,8 @@ fun Top10MoviesList(
                 showItemTitle = !isListFocused,
                 showIndexOverImage = true,
                 onMovieSelected = onMovieClick,
-                onMovieFocused = { selectedMovie = it },
-                modifier = Modifier.onFocusChanged {
-                    isListFocused = it.hasFocus
-                }
+                onMovieFocused = onMovieFocused,
+                modifier = Modifier.onFocusChanged(onFocusChanged)
             )
         }
     }
@@ -153,41 +182,42 @@ private fun MovieDescription(
     }
 }
 
-private fun Modifier.gradientOverlay(gradientColor: Color) = this then drawWithCache {
-    val horizontalGradient = Brush.horizontalGradient(
-        colors = listOf(
-            gradientColor,
-            Color.Transparent
-        ),
-        startX = size.width.times(0.2f),
-        endX = size.width.times(0.7f)
-    )
-    val verticalGradient = Brush.verticalGradient(
-        colors = listOf(
-            Color.Transparent,
-            gradientColor
-        ),
-        endY = size.width.times(0.3f)
-    )
-    val linearGradient = Brush.linearGradient(
-        colors = listOf(
-            gradientColor,
-            Color.Transparent
-        ),
-        start = Offset(
-            size.width.times(0.2f),
-            size.height.times(0.5f)
-        ),
-        end = Offset(
-            size.width.times(0.9f),
-            0f
+private fun Modifier.gradientOverlay(gradientColor: Color): Modifier =
+    drawWithCache {
+        val horizontalGradient = Brush.horizontalGradient(
+            colors = listOf(
+                gradientColor,
+                Color.Transparent
+            ),
+            startX = size.width.times(0.2f),
+            endX = size.width.times(0.7f)
         )
-    )
+        val verticalGradient = Brush.verticalGradient(
+            colors = listOf(
+                Color.Transparent,
+                gradientColor
+            ),
+            endY = size.width.times(0.3f)
+        )
+        val linearGradient = Brush.linearGradient(
+            colors = listOf(
+                gradientColor,
+                Color.Transparent
+            ),
+            start = Offset(
+                size.width.times(0.2f),
+                size.height.times(0.5f)
+            ),
+            end = Offset(
+                size.width.times(0.9f),
+                0f
+            )
+        )
 
-    onDrawWithContent {
-        drawContent()
-        drawRect(horizontalGradient)
-        drawRect(verticalGradient)
-        drawRect(linearGradient)
+        onDrawWithContent {
+            drawContent()
+            drawRect(horizontalGradient)
+            drawRect(verticalGradient)
+            drawRect(linearGradient)
+        }
     }
-}
