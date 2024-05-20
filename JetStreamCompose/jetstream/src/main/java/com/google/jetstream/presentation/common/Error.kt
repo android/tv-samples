2643026efa6b98fab0c98591a322911c3f4b1424
Chiko Shimizu <chikoski@google.com>
2024-05-20 15:09:02 +0900

    Changed to refer to strings.xml from the actual string values.

--- JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/common/Error.kt
+++ JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/common/Error.kt
@@ -18,9 +18,14 @@ package com.google.jetstream.presentation.common
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.tv.material3.Text
+import com.google.jetstream.R
 
 @Composable
 fun Error(modifier: Modifier = Modifier) {
-    Text(text = "Wops, something went wrong...", modifier = modifier)
+    Text(
+        text = stringResource(id = R.string.message_error),
+        modifier = modifier
+    )
 }
--- JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/common/Loading.kt
+++ JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/common/Loading.kt
@@ -20,9 +20,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Text
+import com.google.jetstream.R
 
 @Composable
 fun Loading(
@@ -30,6 +32,9 @@ fun Loading(
     style: TextStyle = MaterialTheme.typography.displayMedium
 ) {
     Box(modifier = modifier, contentAlignment = Alignment.Center) {
-        Text(text = "Loading...", style = style)
+        Text(
+            text = stringResource(id = R.string.message_loading),
+            style = style
+        )
     }
 }
