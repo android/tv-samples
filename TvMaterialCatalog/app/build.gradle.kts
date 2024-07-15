ddccb3138d27bec64dab442d421ea450c068a0fc
Paul Lammertsma <plammer@google.com>
2024-07-15 15:07:14 +0530

    use debug keystore for release

--- TvMaterialCatalog/app/build.gradle.kts
+++ TvMaterialCatalog/app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
